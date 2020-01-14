b1871fdb2f8b09c7dfef6b199d33bed057d4fa4f
UMASHIBA <umashiba1@gmail.com>
2020-01-14 20:56:44 +0900

    [MOD]webpackでホットリロードするようにした。

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,32 @@
-const path = require('path');
+const path = require("path");
+
+const outputPath = path.resolve(__dirname, "public");
+
 // []webpackを使いたかったらyarn webpackでいける
 module.exports = {
-    mode: 'development',
-    entry: './src/index.ts',
-    output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname, 'public/js')
-    },
-    module: {
-        rules: [{
-            test: /\.ts/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        }, ],
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
+  mode: "development",
+  entry: "./src/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: `${outputPath}/js`
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  devServer: {
+    contentBase: `${outputPath}/`,
+    open: true,
+    hot: true,
+    inline: true,
+    watchContentBase: true
+  }
 };
