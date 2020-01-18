d26629b54b3c09d3907bc86a8c12adfa748dd840
UMASHIBA <umashiba1@gmail.com>
2020-01-19 06:06:00 +0900

    [ADD] lightを追加

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,15 @@ import createCamera from "./3d/createCamera";
 import createRenderer from "./3d/createRenderer";
 import createScene from "./3d/createScene";
 import BGBlock from "./3d/component/BGBlock";
+import createLight from "./3d/createLight";
 
 document.addEventListener("DOMContentLoaded", () => {
   controllGeneral();
   const camera = createCamera();
   const renderer = createRenderer();
   const scene = createScene();
+  const light = createLight();
+  scene.add(light);
 
   const bgBlock = new BGBlock();
   scene.add(bgBlock);
