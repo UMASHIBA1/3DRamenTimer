618012fca354204e19e49ae1d6910bfc19e399ab
UMASHIBA <umashiba1@gmail.com>
2020-01-20 12:37:00 +0900

    [MOD]BGBlockのアニメーションを作成

--- src/3d/component/BGBlock.ts
+++ src/3d/component/BGBlock.ts
@@ -9,10 +9,27 @@ class BGBlock extends THREE.Mesh {
     const width = window.innerWidth;
     this.position.x = (Math.random() - 0.5) * width;
     this.position.y = (Math.random() - 0.5) * height;
-    this.position.z = Math.random() * 1000;
+    this.position.z = Math.random() * 700;
     this.rotation.x = Math.random() * 2 * Math.PI;
     this.rotation.y = Math.random() * 2 * Math.PI;
     this.rotation.z = Math.random() * 2 * Math.PI;
+  }
+
+  private _fallDown() {
+    const height = window.innerHeight;
+    this.position.y =
+      this.position.y > -(height / 2)
+        ? this.position.y - 0.02
+        : height / 2 - 60;
+  }
+
+  private _rotate() {
+    this.rotation.y = (this.rotation.y + 0.01) % (Math.PI * 2);
+  }
+
+  public tick() {
+    this._rotate();
+    this._fallDown();
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,32 @@ import createScene from "./3d/createScene";
 import BGBlock from "./3d/component/BGBlock";
 import createLight from "./3d/createLight";
 
+const bgBlocksTick = (bgBlockList: BGBlock[]) => {
+  for (let bgBlock of bgBlockList) {
+    bgBlock.tick();
+  }
+};
+
 document.addEventListener("DOMContentLoaded", () => {
+  const bgBlocksList: BGBlock[] = [];
   controllGeneral();
   const camera = createCamera();
   const renderer = createRenderer();
   const scene = createScene();
   const light = createLight();
 
+  const tick = () => {
+    bgBlocksTick(bgBlocksList);
+    renderer.render(scene, camera);
+    requestAnimationFrame(tick);
+  };
+
   scene.add(light);
-  for (let i = 0; i < 200; i++) {
+  for (let i = 0; i < 150; i++) {
     const bgBlock = new BGBlock();
+    bgBlocksList.push(bgBlock);
     scene.add(bgBlock);
   }
 
-  renderer.render(scene, camera);
+  tick();
 });
