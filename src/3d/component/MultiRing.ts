c31afc3ac187cfc71ef73f102f9f028f8215deeb
UMASHIBA <umashiba1@gmail.com>
2020-02-12 02:39:13 +0900

    [ADD]MultiRingを作成

---
+++ src/3d/component/MultiRing.ts
@@ -0,0 +1,41 @@
+import * as THREE from "three";
+import OneRing from "./OneRing";
+
+class MultiRing {
+  private _group: THREE.Group;
+  private _oneRings: OneRing[];
+  constructor(
+    outlineRadius: number = 50,
+    ringNum: number = 3,
+    ringSizeDiff: number = 10
+  ) {
+    this._group = new THREE.Group();
+    this._oneRings = [];
+
+    for (
+      let [count, nowRadius] = [0, outlineRadius];
+      count < ringNum;
+      count++, nowRadius -= ringSizeDiff
+    ) {
+      const rotateDirection = count % 2 ? "left" : "right";
+      const ring = new OneRing(rotateDirection, nowRadius);
+      this._oneRings.push(ring);
+    }
+
+    for (let i of this._oneRings) {
+      this._group.add(i.ring);
+    }
+  }
+
+  public get multiRing() {
+    return this._group;
+  }
+
+  public tick() {
+    for (let i of this._oneRings) {
+      i.tick();
+    }
+  }
+}
+
+export default MultiRing;
