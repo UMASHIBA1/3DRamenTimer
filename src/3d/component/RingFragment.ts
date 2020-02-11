e01a97a1ea492b75c5b1ef2be96aa891daefc215
UMASHIBA <umashiba1@gmail.com>
2020-02-12 02:48:43 +0900

    [MOD]RingFragmentがtubeを引数として受け取るように変更

--- src/3d/component/RingFragment.ts
+++ src/3d/component/RingFragment.ts
@@ -1,12 +1,12 @@
 import * as THREE from "three";
 
 class RingFragment extends THREE.Mesh {
-  constructor(fragmentIndex: number, radius: number) {
+  constructor(fragmentIndex: number, radius: number, tube: number = 3) {
     // ring1周でarcの値は6.3となる
     // 6.3を9で割り0.7、それに0.9を掛け0.63。残りの0.07は空白
     const thisAngleDiff = (2 / 9) * Math.PI * fragmentIndex;
     const circleDiameter = 10;
-    const geometry = new THREE.TorusGeometry(radius, 3, 16, 100, 0.63);
+    const geometry = new THREE.TorusGeometry(radius, tube, 16, 100, 0.63);
     const material = new THREE.MeshToonMaterial({ color: "#FF0000" });
     super(geometry, material);
     this.position.x = Math.sin(thisAngleDiff) * circleDiameter;
