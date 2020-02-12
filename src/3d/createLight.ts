7f213c0dccaf023eca18ca9a22f76d60d660efc6
UMASHIBA <umashiba1@gmail.com>
2020-02-13 07:15:14 +0900

    [MOD]lightの方向を変更

--- src/3d/createLight.ts
+++ src/3d/createLight.ts
@@ -2,6 +2,6 @@ import * as THREE from "three";
 
 export default () => {
   const light = new THREE.DirectionalLight(0xffffff);
-  light.position.set(1, 1, 1);
+  light.position.set(0, 1, 1);
   return light;
 };
