18218d79dd7b4c983096acd79c26905e31729251
UMASHIBA <umashiba1@gmail.com>
2020-02-19 10:41:31 +0900

    [MOD]MyCameraに画面の上下にカメラを移動する機能を作成

--- src/3d/MyCamera.ts
+++ src/3d/MyCamera.ts
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import easing from "./system/easing";
 
 const resizeCameraAspect = (camera: THREE.PerspectiveCamera) => {
   const width = window.innerWidth;
@@ -7,11 +8,16 @@ const resizeCameraAspect = (camera: THREE.PerspectiveCamera) => {
   camera.updateProjectionMatrix();
 };
 
+const risedLocation = 500;
+const defaultLocation = 0;
 const cameraPositionZ = 500;
+
 class MyCamera {
   private _camera: THREE.PerspectiveCamera;
+  private _transitionDirection: "rise" | "descent" | "stopping";
   constructor() {
     this._camera = new THREE.PerspectiveCamera(45);
+    this._transitionDirection = "stopping";
     this._camera.position.set(0, 0, cameraPositionZ);
     // this._camera.lookAt(new THREE.Vector3(0, 0, 0));
     resizeCameraAspect(this._camera);
@@ -22,6 +28,38 @@ class MyCamera {
 
   public get camera() {
     return this._camera;
+  }
+
+  public riseCamera() {
+    this._transitionDirection = "rise";
+  }
+
+  public descentCamera() {
+    this._transitionDirection = "descent";
+  }
+
+  public tick() {
+    const easingSpeed = 0.12;
+    const canStopDiff = 0.1;
+    if (this._transitionDirection === "rise") {
+      this._camera.position.y += easing(
+        this._camera.position.y,
+        risedLocation,
+        easingSpeed
+      );
+      if (risedLocation - this._camera.position.y < canStopDiff) {
+        this._transitionDirection = "stopping";
+      }
+    } else if (this._transitionDirection === "descent") {
+      this._camera.position.y += easing(
+        this._camera.position.y,
+        defaultLocation,
+        easingSpeed
+      );
+      if (this._camera.position.y - defaultLocation < canStopDiff) {
+        this._transitionDirection = "stopping";
+      }
+    }
   }
 }
 
